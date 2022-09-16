4ae94c8c3ff6b1ddeeb8b86d75bd52d3d010e707
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-09-16 10:33:31 +0300

    refactoring

---
+++ Excercises/01.CreateTables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `minions`(
+	id INT primary key auto_increment,
+    name VARCHAR(255),
+    age INT
+);
+CREATE TABLE `towns`(
+town_id int primary key auto_increment,
+name VARCHAR(255) NOT NULL
+);
