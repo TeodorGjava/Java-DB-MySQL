078ed5c09856d0bc16b38e05825599e5419b6e01
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-09-16 09:54:55 +0300

    Update CreateTables.sql

--- Lab/CreateTables.sql
+++ Lab/CreateTables.sql
@@ -8,9 +8,24 @@ CREATE TABLE `categories`(
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(50) NOT NULL
 );
+
 CREATE TABLE `products`(
 `id` INT PRIMARY KEY AUTO_INCREMENT,
 `name` VARCHAR(50) NOT NULL,
 `category_id` INT 
 references `id`.`categories`
 );
+insert into `categories`(id,name)values
+ (1,"pesho"),
+ (2,"goshov"),
+ (3,"toshov")
+;
+insert into `employees`(id, first_name, last_name) values
+ (1,"pesho","peshov"),
+ (2,"pesho","goshov"),
+ (3,"pesho","toshov")
+;
+insert into `products`(id,name,category_id)values
+ (1,"pesho",3),
+ (2,"goshov",2),
+ (3,"toshov",1);
