3bdd414ba3dc84e8883d2a49ff70c0d1aa8dadb8
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-09-24 16:36:13 +0300

    Create MixPeaksAndRivers.sql

---
+++ Built-in-Functions/Exercises/MixPeaksAndRivers.sql
@@ -0,0 +1,4 @@
+select `peak_name`, `river_name`, concat(lower(`peak_name`),substring(lower(`river_name`),2))
+as `mix` from `peaks`,`rivers`
+where right(`peak_name`,1) = left(`river_name`,1)
+order by `mix`;
