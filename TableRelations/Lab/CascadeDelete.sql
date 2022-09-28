47fd8a6e98aa0b26bd4ce8fa31911077e43e8b57
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-09-28 19:11:33 +0300

    Create CascadeDelete.sql

---
+++ TableRelations/Lab/CascadeDelete.sql
@@ -0,0 +1,12 @@
+create table mountains(
+`id` int primary key auto_increment,
+`name` varchar(50) not null);
+
+create table peaks(
+`id` int primary key auto_increment,
+`name` varchar(50) not null,
+`mountain_id` int not null,
+constraint fk_peaks_mountains
+foreign key(`mountain_id`)
+references mountains(`id`)
+on delete cascade);
